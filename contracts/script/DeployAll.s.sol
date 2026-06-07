758fcb4a44878e48e898e8fa61efaf979e8a4eda
Hector <thanhnghiep.workspace@gmail.com>
2026-06-07 18:22:08 +0700

    Update README for local running

--- contracts/script/DeployAll.s.sol
+++ contracts/script/DeployAll.s.sol
@@ -9,19 +9,20 @@ contract DeployAllScript is Script {
     MasterNFT public masterNFT;
     FragmentMarketplace public marketplace;
 
-    address public constant MUSEUM_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-
     function setUp() public {}
 
     function run() public {
+        // Fetch museum address from env, or default to Anvil Account #0
+        address museumAddress = vm.envOr("MUSEUM_ADDRESS", address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266));
+
         vm.startBroadcast();
 
         masterNFT = new MasterNFT();
         marketplace = new FragmentMarketplace();
 
         // Grant MUSEUM_ROLE to the Museum Address explicitly on both contracts
-        masterNFT.grantRole(masterNFT.MUSEUM_ROLE(), MUSEUM_ADDRESS);
-        marketplace.grantRole(marketplace.MUSEUM_ROLE(), MUSEUM_ADDRESS);
+        masterNFT.grantRole(masterNFT.MUSEUM_ROLE(), museumAddress);
+        marketplace.grantRole(marketplace.MUSEUM_ROLE(), museumAddress);
 
         vm.stopBroadcast();
     }
